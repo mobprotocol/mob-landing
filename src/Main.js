ff71312419e0cf70af46a27c9959baffb01e27a0
Sean Pollock <seanpollock3344@gmail.com>
2017-07-10 11:59:00 -0400

    Main react component

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Main extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <h1>MOB</h1>
+    )
+  }
+}
