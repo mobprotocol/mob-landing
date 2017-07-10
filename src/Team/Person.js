d932c95d07bdff295ff6cf23bc105ecccc43442f
Sean Pollock <seanpollock3344@gmail.com>
2017-07-10 15:05:53 -0400

    Components for team cards

---
+++ src/Team/Person.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Person extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div style = {{ width: 100, height: 200, color: '#484848', boxShadow: '5px 5px 5px 5px' }}>
+        
+      </div>
+    )
+  }
+}
