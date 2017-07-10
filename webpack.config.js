e5ca24414b1ffb037d7b927c8db12cdb6fca9c09
Sean Pollock <seanpollock3344@gmail.com>
2017-07-10 09:59:39 -0400

    webpack setup with babel and css loader

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+  entry: __dirname + '/index.js',
+  output: {
+    path: __dirname,
+    filename: 'dist.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.(js)?$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-2']
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader']
+      }
+    ]
+}
