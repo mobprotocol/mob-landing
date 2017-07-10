7a91edbdd790e6b1ef9dcaa3cc212b08229f8b3e
Sean Pollock <seanpollock3344@gmail.com>
2017-07-10 15:04:14 -0400

    font styling

--- src/Main.js
+++ src/Main.js
@@ -5,7 +5,8 @@ import {
   Col
 } from 'react-bootstrap'
 
-import css from './index.css'
+import Navigation from './Navigation'
+import Person from './Team/Person'
 
 export default class Main extends Component {
   constructor() {
@@ -21,18 +22,20 @@ export default class Main extends Component {
                 <img src='./src/imgs/mob_logo.png' />
               </div>
           </Col>
-          <Col sm={6} style={{ height: 500 }}>
+          <Col sm={6} style={{ height: 500, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
               <div style={{ height: 200, color: '#484848' }}>
-                <h1>
+                <h1 style={{ fontWeight: 900 }}>
                   m o b
                 </h1>
-                <h3>
+                <h3 style={{ fontWeight: 300, paddingBottom: 5 }}>
                   ERC20 exchange network
                 </h3>
+                <Navigation />
               </div>
           </Col>
         </Row>
         <Row style={{ height: 400, backgroundColor: '#484848' }}>
+          <Person />
         </Row>
       </div>
     )
