3a51ca16bf4c3fc069b920104db985f0cdcdb6cb
Sean Pollock <seanpollock3344@gmail.com>
2017-07-10 15:48:32 -0400

    Adding team section with person cards

--- src/Team/Person.js
+++ src/Team/Person.js
@@ -7,8 +7,10 @@ export default class Person extends Component {
 
   render() {
     return (
-      <div style = {{ width: 100, height: 200, color: '#484848', boxShadow: '5px 5px 5px 5px' }}>
-        
+      <div style = {{ width: 250, height: 300, color: 'white' }}>
+        <img src='./src/imgs/sean_team.JPG' />
+        <h4 style={{ fontWeight: 200 }}>Sean Pollock | Engineer</h4>
+        <p></p>
       </div>
     )
   }
--- src/Team/Team.js
+++ src/Team/Team.js
@@ -1,7 +1,40 @@
 import React, { Component } from 'react'
+import {
+  Grid,
+  Row,
+  Col
+} from 'react-bootstrap'
+
+import Person from './Person'
+
+const person_style = {
+  isplay: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
 
 export default class Team extends Component {
   constructor() {
     super()
   }
+
+  render() {
+    return (
+      <Grid>
+        <h2 style={{ color: 'white', fontWeight: '200', paddingBottom: 15 }}>TEAM</h2>
+        <Row>
+          <Col sm={4} style={person_style}>
+            <Person name='Sean Pollock'/>
+          </Col>
+          <Col sm={4} style={person_style}>
+
+          </Col>
+          <Col sm={4} style={person_style}>
+
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+
 }
