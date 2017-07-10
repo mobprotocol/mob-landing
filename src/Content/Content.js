f7daee7541416bbb572c898ecc0fbf75d7e7c86a
Sean Pollock <seanpollock3344@gmail.com>
2017-07-10 16:58:51 -0400

    Grid for content layout

---
+++ src/Content/Content.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {
+  Grid,
+  Row,
+} from 'react-bootstrap'
+
+export default class Content extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <Grid style={{ backgroundColor: '#F5F5F5' }}>
+      </Grid>
+    )
+  }
+}
